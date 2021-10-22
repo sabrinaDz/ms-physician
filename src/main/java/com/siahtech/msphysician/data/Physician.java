5c112192f28260ae808254eff930d59d789af302
sabrinaDz <sabrinasahli@live.fr>
2021-10-22 06:50:23 -0700

    #2 Create Physician Model

---
+++ src/main/java/com/siahtech/msphysician/data/Physician.java
@@ -0,0 +1,33 @@
+package com.siahtech.msphysician.data;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Physician {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private long tel;
+    private long positionId;
+    private UUID departmentId;
+    private LocalDate startDateActivity;
+
+
+
+}
---
+++ src/main/java/com/siahtech/msphysician/repository/PhysicianRepository.java
@@ -0,0 +1,15 @@
+package com.siahtech.msphysician.repository;
+
+import com.siahtech.msphysician.data.Physician;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.UUID;
+
+@Repository
+public interface PhysicianRepository extends JpaRepository<UUID, Physician> {
+
+    Physician findByPositionId(UUID positionId);
+    Physician findByDepartmentId(UUID departmentId);
+}
