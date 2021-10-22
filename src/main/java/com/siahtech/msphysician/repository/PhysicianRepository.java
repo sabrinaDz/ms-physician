d1e59f84cf1c2f938b0562fd428359fb05ed071f
sabrinaDz <sabrinasahli@live.fr>
2021-10-22 07:01:48 -0700

    #3 create Physician Repository

--- src/main/java/com/siahtech/msphysician/repository/PhysicianRepository.java
+++ src/main/java/com/siahtech/msphysician/repository/PhysicianRepository.java
@@ -3,7 +3,6 @@ package com.siahtech.msphysician.repository;
 import com.siahtech.msphysician.data.Physician;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.UUID;
 
