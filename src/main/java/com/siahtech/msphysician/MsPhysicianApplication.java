6618d85a97a350ced08b601b2aa3ba960b6846a4
sabrinaDz <sabrinasahli@live.fr>
2021-10-22 06:06:42 -0700

    #Init project

---
+++ src/main/java/com/siahtech/msphysician/MsPhysicianApplication.java
@@ -0,0 +1,13 @@
+package com.siahtech.msphysician;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPhysicianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPhysicianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/siahtech/msphysician/MsPhysicianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siahtech.msphysician;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPhysicianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
